8d1bff45fd1fb3085fa48e72e3e6c239e8d14ce4
Larissa Manrique <larissasantos200702@gmail.com>
2023-09-25 11:54:11 -0300

    Cria função para limpar formulario no arquivo js

---
+++ controller.js
@@ -0,0 +1,9 @@
+'use restrict'; //Modo restrito
+
+//Limpar formulário
+const LimparFormulario = (endereco) =>{
+    document.getElementById('rua').value = '';
+    document.getElementById('estado').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('estado').value = '';
+}
